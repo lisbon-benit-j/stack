d8de23042c78d232029b8ce49b2370650a8e0da1
lisbon-benit-j <benitjack07@gmail.com>
2026-01-06 23:17:41 +0530

    Remove comments from stockRoutes.js

--- backend/routes/stockRoutes.js
+++ backend/routes/stockRoutes.js
@@ -6,10 +6,10 @@ const {
   stockOut,
 } = require("../controllers/stockController");
 
-// STOCK IN
+
 router.post("/in", stockIn);
 
-// STOCK OUT
+
 router.post("/out", stockOut);
 
 module.exports = router;
